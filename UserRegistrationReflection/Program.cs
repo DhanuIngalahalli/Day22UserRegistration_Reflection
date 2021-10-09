76fa0458acec22f9ebecbbbe49ff87aed30d23b2
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2021-10-09 10:19:44 -0700

    Welcome to User Registration using Reflection

---
+++ UserRegistrationReflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User registration Program using Reflection ");
+
+            UseReflection.ReflectionTest();
+        }
+    }
+}
